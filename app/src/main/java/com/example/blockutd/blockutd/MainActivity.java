ff59c8687ebeea5ca4bfbc9fb888a82c6f869244
Derek Li <derek_t_li@tamu.edu>
2018-02-25 01:45:11 -0600

    idk what im doing
    something with my stuff

--- app/src/main/java/com/example/blockutd/blockutd/MainActivity.java
+++ app/src/main/java/com/example/blockutd/blockutd/MainActivity.java
@@ -1,24 +1,42 @@
 package com.example.blockutd.blockutd;
 
 import android.app.AppOpsManager;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Process;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
 
 import static android.app.AppOpsManager.MODE_ALLOWED;
 import static android.app.AppOpsManager.OPSTR_GET_USAGE_STATS;
 
 public class MainActivity extends AppCompatActivity {
 
+    private long endTime;
+    private long beginTime;
+    private List<UsageStats> stats;
+
+
+
+    private UsageStatsManager usageStatsManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        endTime = System.currentTimeMillis();
+        beginTime = System.currentTimeMillis()-1000*60*60*3; //3 hours ago
     }
     private boolean checkForPermission(Context context) {
         AppOpsManager appOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
         int mode = appOps.checkOpNoThrow(OPSTR_GET_USAGE_STATS, Process.myUid(), context.getPackageName());
+        usageStatsManager = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+
+        List<UsageStats> stats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_WEEKLY, beginTime,
+        endTime);
+
         return mode == MODE_ALLOWED;
     }
 }
