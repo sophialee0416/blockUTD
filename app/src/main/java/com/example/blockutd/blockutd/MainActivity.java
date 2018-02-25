10c2450bb30e03074f46fb034dfe4f907e024a84
Derek Li <derek_t_li@tamu.edu>
2018-02-25 00:21:14 -0600

    i changed stuff
    howdy ags!

--- app/src/main/java/com/example/blockutd/blockutd/MainActivity.java
+++ app/src/main/java/com/example/blockutd/blockutd/MainActivity.java
@@ -3,11 +3,29 @@ package com.example.blockutd.blockutd;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+
+    static final String URL = "https://api.androidhive.info/pizza/?format=xml";
+
+
+    // XML node keys
+    static final String KEY_ITEM = "item";
+    static final String KEY_ID = "id";
+    static final String KEY_NAME = "name";
+    static final String KEY_COST = "cost";
+    static final String KEY_DESC = "description";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<HashMap<String, String>> menuItems = new ArrayList<>();
+
+
     }
 }
---
+++ app/src/main/java/com/example/blockutd/blockutd/XMLParser.java
@@ -0,0 +1,16 @@
+package com.example.blockutd.blockutd;
+
+/**
+ * Created by Derek on 2/24/2018.
+ */
+
+public class XMLParser {
+
+    public String getXMLFromUrl(String url){
+        String xml = null;
+
+        try(
+                Default
+                )
+    }
+}
