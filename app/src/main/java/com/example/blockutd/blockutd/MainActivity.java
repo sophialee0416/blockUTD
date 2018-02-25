8c78dc6dc1e575ea67690db10f7001615bd013b5
Derek Li <derek_t_li@tamu.edu>
2018-02-25 01:14:53 -0600

    nice
    im howdy

--- app/src/main/java/com/example/blockutd/blockutd/MainActivity.java
+++ app/src/main/java/com/example/blockutd/blockutd/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.blockutd.blockutd;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AppOpsManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Process;
+import android.support.v7.app.AppCompatActivity;
+
+import static android.app.AppOpsManager.MODE_ALLOWED;
+import static android.app.AppOpsManager.OPSTR_GET_USAGE_STATS;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private boolean checkForPermission(Context context) {
+        AppOpsManager appOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+        int mode = appOps.checkOpNoThrow(OPSTR_GET_USAGE_STATS, Process.myUid(), context.getPackageName());
+        return mode == MODE_ALLOWED;
     }
 }
